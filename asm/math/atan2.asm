96743b31b702c7e6a673d6c67fab3c8045f4f2ba
Vitor Vilela <vitor.vilela.mario@gmail.com>
2025-08-09 23:34:47 -0300

    Fix dividend check on atan2.asm, fixes #1
    Updated from https://bin.smwcentral.net/u/8691/Akagin2GetAtan2.asm

--- asm/math/atan2.asm
+++ asm/math/atan2.asm
@@ -1,100 +1,132 @@
-; by Akaginite
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; GetAtan2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 ; This routine will get angle between two points.
-;
-; Input:
+; If two points distances are far than 2^(n+8),
+; calculation accuracy will loss to 1/(n+1).
+; 
+; input:
 ; $00-$01 X-Distance
 ; $02-$03 Y-Distance
 ;
-; Output:
-; $00-$01 Angle (0x0000-0x01FF)
-; $02-$06 will be destructed.
+; output:
+; $04-$05 Angle (0x0000-0x01FF)
+;
+; destroy:
+; $00-$03, $06
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-GetAtan2:	REP #$20
-		LDY #$00
-		LDA $00			;\
-		BPL .X_Plus		; |
-		LDY #$04		; |
-		EOR.w #$FFFF		; | Get absolute value of X
+		PHX
+		PHP
+		SEP #$10
+		REP #$20
+		LDX #$00		;\
+		LDA $02			; |
+		BPL ?+			; | Get absolute value of Y-Distance
+		EOR.w #$FFFF		; | Reduce angle from 0-359 to 0-179 degrees
 		INC A			; |
-		STA $00			;/
-.X_Plus		LDA $02			;\
-		BPL .Y_Plus		; |
-		INY #2			; |
-		EOR.w #$FFFF		; | Get absolute value of Y
-		INC A			; |
-		STA $02			;/
-.Y_Plus		CMP $00			;\ If X is smaller than Y...
-		BCC +			;/
-		STA $04			;\
-		LDA $00			; |
-		STA $02			; | swap X and Y.
-		LDA $04			; |
+		STA $02			; |
+		LDX #$04		;/
+?+		LDA $00			;\
+		BPL ?+			; |
+		EOR.w #$FFFF		; | Get absolute value of X-Distance
+		INC A			; | Reduce angle from 0-179 to 0-89 degrees
 		STA $00			; |
-		INY			;/
-+		STY $06			;
-		LDA $02			;
-		STA $04			;
-		STZ $02			;
-		ORA $00			;
-		AND.w #$FF00		;
-		BEQ .Next		;
-		XBA			;
--		LSR $00			;
-		LSR $04			;
-		ROR $02			;
-		LSR A			;
-		BNE -			;
-.Next		LDY $00			;
-		BEQ .DivZero		;
-		LDA $03			;
-		STA $4204		;
-		STY $4206		;
-		SEP #$20		;\
-		STZ $01			; |
-		LDA #$40		; | Wait 16 cycles...
-		CPY $03			; |
-		BEQ +			; |
-		BRA $00			;/
-		LDY $4214		;
-		LDA.w AtanTable,y	;
-+		LSR $06			;
-		BCC +			;
-		EOR #$7F		;
-		INC A			;
-+		LSR $06			;
-		BCC +			;
-		EOR #$FF		;
-		INC A			;
-		BEQ +			;
-		INC $01			;
-+		LSR $06			;
-		BCC +			;
-		EOR #$FF		;
-		INC A			;
-		BNE +			;
-		INC $01			;
-+		STA $00			;
+		INX #2			;/
+?+		CMP $02			;\
+		BCS ?+			; |
+		STA $04			; |
+		LDA $02			; | if |Y| bigger than |X|, swap them
+		STA $00			; | Reduce angle from 0-89 to 0-45 degrees
+		LDA $04			; |
+		STA $02			; |
+		INX			;/
+?+		STX $06			;	
+		
+		LDA $02			;\
+		STA $04			; |
+		STZ $02			; |
+		ORA $00			; |
+		AND.w #$FF00		; |
+		BEQ .Next		; | for reason on the hardware register bit limitation,
+		XBA			; | distance values will trunc to half unless both got below 0x0100...
+.TruncPrecise	LSR $00			; |
+		LSR $04			; |
+		ROR $02			; |
+		LSR A			; |
+		BNE .TruncPrecise	;/
+		
+.Next
+		if !SA1
+			LDX #$01		;
+			STX $2250		;
+			LDA $03			;
+			STA $2251		;
+			LDA $00			;
+			BEQ .DivZero		;
+			STA $2253		;
+			SEP #$20		; 3
+			STZ $05			; 6
+			LDX $2306		; 9 (before read the result)
+			LDA $2307		;
+			BEQ ?+			;
+			LDX #$FF		;
+	?+		LDA.l .AtanTable,x	;
+		else
+			LDX $00			;
+			BEQ .DivZero		;
+			LDA $03			;
+			STA $4204		;
+			STX $4206		;
+			SEP #$20		; 3
+			STZ $05			; 6
+			LDA #$40		; 8
+			CPX $04			; 11
+			BEQ ?+			; 13
+			LDX $4214		; 16 (before read the result)
+			LDA.l .AtanTable,x	;
+		endif
+		
+?+		LSR $06			;\
+		BCC ?+			; | Restore angle from 0-45 to 0-89 degrees
+		EOR #$7F		; |
+		INC A			;/
+?+		LSR $06			;\
+		BCC ?+			; |
+		EOR #$FF		; | Restore angle from 0-89 to 0-179 degrees
+		INC A			; |
+		BNE ?+			; |
+		INC $05			;/
+?+		LSR $06			;\
+		BCC ?+			; |
+		EOR #$FF		; | Restore angle from 0-179 to 0-359 degrees
+		INC A			; |
+		BEQ ?+			; |
+		INC $05			;/
+?+		STA $04			;
 		LDA #$FE		;
-		TRB $01			;
-		RTS			;
+		TRB $05			;
+		PLP			;
+		PLX			;
+		RTL			;
 		
 .DivZero	SEP #$20		;
-		LDY #$00		;
+		LDX #$80		;
 		LDA $06			;
 		LSR A			;
-		BCS +			;
-		LDY #$80		;
+		BCC ?+			;
+		LDX #$00		;
 		LSR A			;
-+		AND #$01		;
-		STA $01			;
-		STY $00			;
-		RTS
+?+		AND #$01		;
+		STA $05			;
+		STX $04			;
+		PLP			;
+		PLX			;
+		RTL			;
 
-AtanTable:
+; ArcTan Lookup Table
+; 256 bytes, 0-45 degrees
+.AtanTable
 db $00,$00,$01,$01,$01,$02,$02,$02,$03,$03,$03,$03,$04,$04,$04,$05
 db $05,$05,$06,$06,$06,$07,$07,$07,$08,$08,$08,$09,$09,$09,$0A,$0A
 db $0A,$0A,$0B,$0B,$0B,$0C,$0C,$0C,$0D,$0D,$0D,$0E,$0E,$0E,$0E,$0F
